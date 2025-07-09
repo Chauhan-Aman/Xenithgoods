dfddfbfb39d6ab572a777232e40a80de50c0288b
Aman Chauhan <129165081+Chauhan-Aman@users.noreply.github.com>
2025-07-09 12:22:58 +0530

    Vercel Insights

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Footer from '../components/Footer'
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import LoadingBar from 'react-top-loading-bar'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
